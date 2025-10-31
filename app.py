f13593e4c60d26b500315e29a4b95ef6f9a533b4
chethuphotosregmail <chethuphotosre@gmail.com>
2025-10-31 11:08:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,4 +65,5 @@ def predict():
         return render_template("result.html", result=f"❌ Error: {str(e)}")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
+
